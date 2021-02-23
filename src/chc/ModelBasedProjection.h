2f7d4eec049358e6e3bf2c24162e8a79e9da1a9e
Martin Blicha <martin.blicha@usi.ch>
2021-02-23 19:45:04 +0100

    initial version of Spacer algorithm

---
+++ src/chc/ModelBasedProjection.h
@@ -0,0 +1,34 @@
+//
+// Created by Martin Blicha on 06.03.21.
+//
+
+#ifndef OPENSMT_MODELBASEDPROJECTION_H
+#define OPENSMT_MODELBASEDPROJECTION_H
+
+#endif //OPENSMT_MODELBASEDPROJECTION_H
+
+#include "PTRef.h"
+#include "Model.h"
+
+#include <unordered_set>
+#include <iosfwd>
+class Logic;
+
+class ModelBasedProjection {
+private:
+    Logic & logic;
+
+public:
+    explicit ModelBasedProjection(Logic & logic) : logic(logic) {}
+
+    PTRef project(PTRef fla, vec<PTRef> const& varsToEliminate, Model& model);
+
+private:
+    using implicant_t = std::unordered_set<PtAsgn, PtAsgnHash>;
+
+    implicant_t projectSingleVar(PTRef var, implicant_t implicant, Model & model);
+
+    implicant_t getImplicant(PTRef var, Model & model);
+
+    void dumpImplicant(ostream& out, implicant_t const & implicant);
+};
